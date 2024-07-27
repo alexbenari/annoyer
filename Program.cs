bd7ec4e072d4300d269ec7c4c78221f96162ad8b
alexbenari <alexbenari@yahoo.com>
2024-07-27 18:04:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+public class Program
+{
+    static void Main()
+    {
+        Infinite_dots("ha ");
+    }
+
+    static void Infinite_dots(string whattoprint=".")
+    {
+        Console.WriteLine("Hello, CHILD!");
+        Console.WriteLine("I HAVE COME FOR YOUR COMPUTER!");
+
+        // Print periods with 1-second intervals
+        while (true) 
+        {
+            Thread.Sleep(1000); // Wait for 1 second
+            Console.Write(whattoprint);
+        }
+    }
+}
